45707ac103c32fda96ecb79f0193c0152afd8a50
Gourav Kumar <gourav.kumar@greenstory.io>
2025-04-03 16:44:04 +0530

    deploying from branch

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://GouravChanalia.github.io/resume',
+  base: '/building-without-routing/',
   build: {
     outDir: 'build'
   }
