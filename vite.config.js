bcc91c3aa10afdaf3375cf02c485437dfad40697
Gourav Kumar <gourav.kumar@greenstory.io>
2025-04-03 16:58:04 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/building-without-routing/',
+  base: 'https://GouravChanalia.github.io/resume/building-without-routing/',
   build: {
     outDir: 'build'
   }
